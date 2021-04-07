c8e7c306349478161de0fd68855b024a387ddc22
Mateus Fonseca <mateusrfon@gmail.com>
2021-04-07 16:56:48 -0300

    correção bug virar 3a carta rapidamente

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let moves = 0;
 let firstCard;
 let secondCard;
 let points = 0;
+let ready = true;
 const images = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif']
 
 Start();
@@ -18,20 +19,27 @@ function Start() {
 
 function DrawCards() {
     const table = document.querySelector('.container')
-    for (i = 0; i < cards; i++) {
+    for (i = 0; i < (cards/2); i++) {
         table.innerHTML += `<div class="card card${i}" onclick="FlipCard('.card${i}')">
         <div class="front-face face"><img src="images/front.png" alt=""></div>
-        <div class="back-face face"><img src="images/metalparrot.gif" alt=""></div> 
-        </div>`; //${images[i]}
+        <div class="back-face face"><img src="images/${images[i]}" alt=""></div> 
+        </div>` + 
+        `<div class="card card${(i + cards/2)}" onclick="FlipCard('.card${(i + cards/2)}')">
+        <div class="front-face face"><img src="images/front.png" alt=""></div>
+        <div class="back-face face"><img src="images/${images[i]}" alt=""></div> 
+        </div>`;
     }
 }
 
 function FlipCard(indice) {
-    const getCard = document.querySelector(indice);
-    if (getCard.querySelector('.flip') == null) {
-        getCard.querySelector('.front-face').classList.add('flip');
-        getCard.querySelector('.back-face').classList.add('flip');
-        CardCheck(getCard);
+    if (ready === true) {
+        const getCard = document.querySelector(indice);
+        if (getCard.querySelector('.flip') == null) {
+            ready = false;
+            getCard.querySelector('.front-face').classList.add('flip');
+            getCard.querySelector('.back-face').classList.add('flip');
+            CardCheck(getCard);
+        }
     }
 }
 
@@ -39,14 +47,15 @@ function CardCheck(card) {
     cardClick++;
     moves++;
     if (cardClick === 1) {
-        firstCard = card;        
+        firstCard = card;
+        ready = true;        
     } else {
         secondCard = card;
         cardClick = 0;
         if (firstCard.querySelector('.back-face').innerHTML === secondCard.querySelector('.back-face').innerHTML) {
             points += 2;
-            //if points = cards : WIN -> "Você ganhou em ${moves} jogadas"
             WinCheck();
+            ready = true;
             /*Bônus 2 = perguntas se gostaria de reiniciar a partida, se sim, 
             zerar variáveis, zerar container e chamar a função start*/
         } else {
@@ -60,11 +69,12 @@ function UnFlip() {
     firstCard.querySelector('.back-face').classList.remove('flip')
     secondCard.querySelector('.front-face').classList.remove('flip')
     secondCard.querySelector('.back-face').classList.remove('flip')
+    ready = true;
 }
 
 function WinCheck() {
     if (points === cards) {
-        points /= 2;
-        alert(`Você ganhou em ${points} jogadas`);
+        moves /= 2;
+        setTimeout('alert(`Você ganhou em ${moves} jogadas`)', 100);
     }
 }
